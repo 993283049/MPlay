ed0ce0104166b6e8be77a258fb554e3f9c009447
FordTan <993283049@qq.com>
2023-03-28 23:02:59 +0800

    Created Project MPlay

---
+++ MPlay/MPlay/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MPlay
+//
+//  Created by FordTan on 2023/3/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MPlay/MPlay/MPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPlayApp.swift
+//  MPlay
+//
+//  Created by FordTan on 2023/3/28.
+//
+
+import SwiftUI
+
+@main
+struct MPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
